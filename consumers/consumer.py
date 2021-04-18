e4bd7269b12776838e2b02af2ff39b66c1581014
Nagy_STCS <ndawod.c@stcs.com.sa>
2021-04-18 18:16:57 +0300

    Adding on_assign to consumer.py subscribe call

--- consumers/consumer.py
+++ consumers/consumer.py
@@ -61,7 +61,7 @@ class KafkaConsumer:
         # how the `on_assign` callback should be invoked.
         #
         #
-        self.consumer.subscribe([topic_name_pattern])
+        self.consumer.subscribe([topic_name_pattern], on_assign=self.on_assign)
 
     def on_assign(self, consumer, partitions):
         """Callback for when topic assignment takes place"""
